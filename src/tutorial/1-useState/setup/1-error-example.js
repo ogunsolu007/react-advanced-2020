cedf52d5c046a28154f3e5afe39cc40da35fe786
electrified007 <63315246+ogunsolu007@users.noreply.github.com>
2022-11-26 22:40:43 +0100

    useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [title, setTitle] = useState("random title");
+
+  const handleClick = () => {
+    setTitle("Hello People");
+    console.log(title);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
